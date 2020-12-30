6bc04ddb24ad6c2b38686584c936a0a4c9259f44
eb110 <wfigura@op.pl>
2020-12-30 20:29:31 +0000

    gwen finished

--- public/scripts/memory.js
+++ public/scripts/memory.js
@@ -1,6 +1,4 @@
 let cards = ["ciri.png", "geralt.png", "jaskier.png", "jaskier.png", "iorweth.png", "triss.png", "geralt.png", "yen.png", "ciri.png", "triss.png", "yen.png", "iorweth.png"]
-//alert(cards[4])
-//console.log(cards)
 
 let individual_cards = [c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11]
 for(let i = 0; i < 12; i++){
@@ -12,11 +10,52 @@ for(let i = 0; i < 12; i++){
 
 let oneVisible = false
 let turnCounter = 0
+let visible_nr
+let lock = false
+let parisLeft = 6
 
-function revealCard(nr)
-{
-   let obraz = `url("../img/${cards[nr]}")`
-   $(`#c${nr}`).css('background-image', obraz)
-   //$(`#c${nr}`).removeClass('card').addClass('card2');
+function revealCard(nr) {
+    let opacityValue = $(`#c${nr}`).css('opacity')
+    if (opacityValue != 0 && lock == false) {
+        lock = true
+        let obraz = `url("../public/img/${cards[nr]}")`
+        $(`#c${nr}`).css('background-image', obraz)
+        $(`#c${nr}`).removeClass('card').addClass('cardA');
+        if (!oneVisible) {
+            //first card
+            oneVisible = true
+            visible_nr = nr
+            lock = false
+        }
+        else {
+            //second card
+            if (cards[visible_nr] === cards[nr]) {
+                setTimeout(() => { hide2Cards(nr, visible_nr) }, 750)
+            }
+            else {
+                setTimeout(() => { restore2Cards(nr, visible_nr) }, 1000)
+            }
+            turnCounter++
+            $('.score').html(`Turn counter: ${turnCounter}`)
+            oneVisible = false
+        }
+    }
+}
 
+function restore2Cards(nr1, nr2){
+    $(`#c${nr1}`).css('background-image', 'url(../public/img/karta.png)')
+    $(`#c${nr1}`).removeClass('cardA').addClass('card');
+    $(`#c${nr2}`).css('background-image', 'url(../public/img/karta.png)')
+    $(`#c${nr2}`).removeClass('cardA').addClass('card');
+    lock = false
+}
+
+function hide2Cards(nr1, nr2){
+    $(`#c${nr1}`).css('opacity', '0')
+    $(`#c${nr2}`).css('opacity', '0')
+    parisLeft--
+    if(parisLeft == 0){
+        $('.board').html(`<h1>You win!<br>Done in ${turnCounter} turns</h1>`)
+    }
+    lock = false
 }
